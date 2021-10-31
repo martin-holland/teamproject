c013c313f5072e7eeca2b9b786d5e68947202b78
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-10-31 15:23:57 +0200

    updated BookForm to set states, currently not working solution

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,10 +4,40 @@ import { getLangs } from './functionsLibrary'
 const languages = getLangs()
 
 
-
 class BookForm extends Component {
-    state = {
-        languages
+    constructor(props) {
+        super(props) 
+        this.state = {
+            languages,
+            owner: "",
+            booktitle: "",
+            author: "",
+            pubYear: "",
+            availability: "",
+            bookLanguage: "",
+            ISBN: "",
+            location: "",
+            adultOrChild: "",
+            ageRange: "",
+            comments: "",
+            }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]:value
+        })
+       
+    }
+
+    logFunction(event) {
+        console.log(this.state)
     }
     
     render() {
@@ -15,44 +45,48 @@ class BookForm extends Component {
             <div className="bookform">
                 <form className = "bookInputForm">
                 <label htmlFor="owner">Owner</label>
-                <input type="text"></input>
+                <input value={this.state.owner}onChange={this.handleInputChange}id="owner"type="text"></input>
                 <label htmlFor="booktitle">Book Title</label>
-                <input type="text"></input>
+                <input value={this.state.booktitle}onChange={this.handleInputChange}type="text"></input>
                 <label htmlFor="author">Author</label>
-                <input type="text"></input>
+                <input value={this.state.author}onChange={this.handleInputChange}type="text"></input>
                 <label htmlFor="year">Publication Year</label>
-                <input type="text"></input>
+                <input value={this.state.pubYear}onChange={this.handleInputChange}type="text"></input>
                 <label htmlFor="availability">Availability</label>
                 <select id="availabilty" name="availability">
-                    <option value="available">Available</option>
-                    <option value="onloan">On Loan</option>
+                    <option value={this.state.availability}onChange={this.handleInputChange}>Available</option>
+                    <option value={this.state.availability}onChange={this.handleInputChange}>On Loan</option>
                 </select>
                     <div className="drop-down">
                         <p>Language</p>
-                            <select>{
+                            <select value={this.state.bookLanguage}onChange={this.handleInputChange}>{
                                 this.state.languages.map((obj) => {
                                 return <option key={obj.name}value={obj.name}>{obj.name}</option>
                                 })
                             }</select>
                     </div>
                 <label htmlFor="ISBN">ISBN</label>
-                <input type="text"></input>
+                <input value={this.state.ISBN}onChange={this.handleInputChange}type="text"></input>
                 <label htmlFor="location">Location</label>
-                <input type="text"></input>
+                <input value={this.state.location}onChange={this.handleInputChange}type="text"></input>
                 <select>
-                    <option value="adult">Adult</option>
-                    <option value="children">Children</option>
+                    <option value={this.state.adultOrChild}onChange={this.handleInputChange}>Adult</option>
+                    <option value={this.state.adultOrChild}onChange={this.handleInputChange}>Children</option>
                 </select>
                 <select>
-                    <option value="0to3">Ages: 0-3</option>
-                    <option value="3to8">Ages: 3-8</option>
-                    <option value="earlyReaders">Early Readers, Ages: 5-9</option>
-                    <option value="firstChaperBooks">First Chapter Books, Ages: 7-10</option>
-                    <option value="middleGrade">Middle Grade Books, Ages: 8-12</option>
-                    <option value="youngAdult">Middle Grade Books, Ages: 12+</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Adult</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Ages: 0-3</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Ages: 3-8</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Early Readers, Ages: 5-9</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>First Chapter Books, Ages: 7-10</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Middle Grade Books, Ages: 8-12</option>
+                    <option value={this.state.ageRange}onChange={this.handleInputChange}>Middle Grade Books, Ages: 12+</option>
                 </select>
                 <label htmlFor="comments">Comments</label>
-                <input type="text"></input>
+                <input value={this.state.comments}onChange={this.handleInputChange}type="text"></input>
+                <button id="bookformsubmit"className="formsubmit" onClick=
+                {this.logFunction()}
+                >Submit</button>
                 </form>
             </div>
         );
