ef328a6c8a4dc5ab3352fc79baa2f73bb491dd43
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-11-11 19:16:56 +0200

    fetchwrapper included

---
+++ src/components/fetchwrapper.js
@@ -0,0 +1,63 @@
+const fetch = require("node-fetch");
+
+const fetchWrapper = {
+  get,
+  post,
+  put,
+  delete: _delete,
+};
+
+function get(url) {
+  const requestOptions = {
+    method: "GET",
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+function post(url, body) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+function put(url, body) {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+// prefixed with underscored because delete is a reserved word in javascript
+function _delete(url) {
+  const requestOptions = {
+    method: "DELETE",
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+// helper functions
+
+function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+
+    if (!response.ok) {
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
+
+async function getData() {
+  let data = await fetchWrapper.get("http://localhost:3000/languages");
+  console.log(data);
+}
+
+console.log(getData());
