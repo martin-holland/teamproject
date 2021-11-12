72673885feae9e64b4d35a5f862c30ec9833cc04
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-12 14:53:46 +0200

    all updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react";
+import React from 'react';
+import logo from './assets/logo.png'; 
+// import logo from './assets/logo.svg';
 
-class Header extends Component {
-  render() {
-    return <div></div>;
-  }
+const Header = () => {
+  return (
+    <header>
+       <div id="logo">
+                    <img src={logo} alt="Logo"/>
+        </div>
+        <h1>The Virtual Shelf</h1>
+        <p>Find your next read from our vide selection of books</p> 
+    </header>
+);
 }
 
 export default Header;
