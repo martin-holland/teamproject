a47d4ce510f6a0ee9f9de6d06b767ceddb2ea07c
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2021-11-15 17:21:50 +0200

    added Search component, testing branches

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <div>
+                <p>SEARCH WILL BE HERE</p>
+            </div>
+        );
+    }
+}
+
+export default Search;
