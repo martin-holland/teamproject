ee14d2d72af5b189ae99996cc0519360d3ad1146
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-19 10:08:10 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,9 @@ const Header = () => {
                     <img src={logo} alt="Logo"/>
         </div>
         <h1>The Virtual Shelf</h1>
-        <p>Find your next read from our vide selection of books</p> 
+        <p>Find your next read from our vide selection of books</p>
+        <br/>
+        <p>It's easy to find books either by language or theme.</p> 
     </header>
 );
 }
