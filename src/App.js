a34c2186613c5ee50712efbf2645f1c5873934f9
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-19 14:36:36 +0200

    about added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import BookForm from './components/BookForm';
+// import BookForm from './components/BookForm';
+import FirebaseForm from './components/FirebaseForm'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
     
     <div>
       <Header />
-      <BookForm />
+      {/* <BookForm /> */}
+      {/* <FirebaseForm /> */}
+      <About />
       <Footer />
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div class="container">
+            <h2>About The Virtual Shelf</h2>
+            <p>The Virtual Shelf is a place where you can find books for yourself and your family to read. You can also share your own books for others to read.</p>
+                
+                <p>The idea for this community is very simple:</p>
+
+
+                <p>If you have any books in your home, which you are not reading anymore you can share them with others. You can either loan them or give them away. The books can be in any language and for kids or adults or anything in between. The main idea is, that no money is changing hands. Submitting your book for loan is simple to do on our site. </p>
+
+                <p>You are able to find books with the help of our search in several languages and plenty of selection for both children and adults. Depending on the agreement with the person who has posted the book, you can either loan it or will not be required to return it. In latter case you can pay it forward and post the book on your profile so someone else can enjoy it.
+                </p>
+            <div className="herobanner">
+            <img src="https://images.unsplash.com/photo-1548048026-5a1a941d93d3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" alt="library shelf"/>
+        </div>
+        </div>
+    );
+};
+
+export default About;
