9f37095636663784aed465a979b1607f23bbc825
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-26 13:01:27 +0200

    created main

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,16 +7,28 @@ const FAQ = () => {
 
             <h3>General</h3>
                 <p>
-                    
+
                 </p>
 
             <h3>Site Usage</h3>
+            <p>
+
+            </p>
 
             <h3>Submitting a Book</h3>
+                <p>
+                    
+                </p>
 
             <h3>Terms and Conditions</h3>
+                <p>
+                    
+                </p>
 
             <h3>Contact Us</h3>
+                <p>
+                    
+                </p>
         </div>
     );
 };
