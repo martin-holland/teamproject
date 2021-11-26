f987ee44d2e58540a66214f4129f141c8cd83e2a
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-26 11:04:02 +0200

    created FAQ

---
+++ src/components/FAQ.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const FAQ = () => {
+    return (
+        <div>
+            <h1>Frequently Asked Questions</h1>
+
+            <h3>General</h3>
+                <p>
+                    
+                </p>
+
+            <h3>Site Usage</h3>
+
+            <h3>Submitting a Book</h3>
+
+            <h3>Terms and Conditions</h3>
+
+            <h3>Contact Us</h3>
+        </div>
+    );
+};
+
+export default FAQ;
