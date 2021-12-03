26046d3719890735ad025f456e15ca3868bf7c6b
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-12-03 12:19:03 +0200

    update before pull request

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
+import "./App.css";
 // import BookForm from './components/BookForm';
-import FirebaseForm from './components/FirebaseForm'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import About from './components/About';
-import Main from './components/Main';
-import Library from './components/Library';
+import FirebaseForm from "./components/FirebaseForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./components/About";
+import Main from "./components/Main";
+import Library from "./components/Library";
+import ClasslessSearch from "./components/ClasslessSearch";
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <FirebaseForm /> */}
+      <FirebaseForm language="italian" />
       <Main />
       <Footer />
+      <ClasslessSearch />
       <Library language="italian" />
     </div>
   );
