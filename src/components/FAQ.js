4dbeb50f96ac7c7ce4d149c4172e6e1d602f75c8
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-12-09 20:40:43 +0200

    updated css and faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,34 +1,27 @@
 import React from 'react';
+import "./FAQ.css";
 
 const FAQ = () => {
     return (
-        <div>
+        <div className="faq">
             <h1>Frequently Asked Questions</h1>
 
             <h3>General</h3>
-                <p>
-
-                </p>
+                <p>Q. What is The Virtual Shelf?</p>
+                <p>A. The Virtual Shelf is an online international bookshelf, where you can place books for others to borrow and from where you cand find your books for yourself.</p>
 
             <h3>Site Usage</h3>
-            <p>
-
-            </p>
+            <p>Q.</p>
+            <p>A.</p>
 
             <h3>Submitting a Book</h3>
-                <p>
-                    
-                </p>
+                <p>Q. How do I submit a book to the Virtual Shelf?</p>
+                <p>A. This has been made as simple as possible. All you have to do is create a username & password after which you submit your book details with the form provided. That's it!</p>
 
             <h3>Terms and Conditions</h3>
-                <p>
-                    
-                </p>
+            <p>Q.</p>
+            <p>A.</p>
 
-            <h3>Contact Us</h3>
-                <p>
-                    
-                </p>
         </div>
     );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
         return (
             <footer>
             <div>
-                <p>The Handlers &copy; 2021</p> 
+                <h4>The Handlers &copy; 2021</h4> 
                 <p>Contact us: 
-                    <a href="tel:+15555551212">555-555-1212</a></p>
-                    
+                    <a href="tel:+15555551212">555-555-1212</a>
+                    </p>
+                   <p> 
+                    <a href="email:info@handlers.com">Mail to: info@handlers.com</a>
+                    </p>
             </div>
             </footer>
         );
