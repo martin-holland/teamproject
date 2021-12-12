046b4aa4ed877106f9d22a2d2e922debdaec9c5d
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-12-12 18:26:30 +0200

    some css

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -11,8 +11,8 @@ const FAQ = () => {
                 <p>A. The Virtual Shelf is an online international bookshelf, where you can place books for others to borrow and from where you cand find your books for yourself.</p>
 
             <h3>Site Usage</h3>
-            <p>Q.</p>
-            <p>A.</p>
+            <p>Q. What personal details are required when creating an account?</p>
+            <p>A. In order to post a book, all that is required is a username and password. When posting a book, it is required to provide the location (city) where the book is located. All communication can be handled within the app, so no phone numbers or e-mail addresses are needed.</p>
 
             <h3>Submitting a Book</h3>
                 <p>Q. How do I submit a book to the Virtual Shelf?</p>
