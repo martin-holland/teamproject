0b98327f881f07da47a296c8c37ef635afb74045
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-12-13 09:37:31 +0200

    stash before commit

--- src/components/BookSingle.js
+++ src/components/BookSingle.js
@@ -1,44 +1,42 @@
-import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const BookSingle = ({
-    id,
-    title,
-    author,
-    available,
-    location,
-    isbn, 
-    age,
-    image
+  id,
+  title,
+  author,
+  available,
+  location,
+  isbn,
+  age,
+  image,
 }) => {
-    let {book} = useParams();
-    // let navigate = useNavigate();
+  let { book } = useParams();
+  // let navigate = useNavigate();
 
-    return (
-        <div className="singleBook">
-            <div className="singleBook">
-            <p>This is the page for a single book with its complete details</p>
-            <p> Book Title: {} </p>
-            <p> Author: {} </p>
-            <p> Book Language: {} </p>
-            <p> Owner: {} </p>
-            <p> ISBN: {} </p>
-            <p> Age Range: {} </p>
-            <p> Available: {} </p>
-            <p> Publication Year: {} </p>
-            <p> Location: {} </p>
-            <p> Comments: {} </p>
-            <button>
-              Request Book
-            </button>
-            </div>
-            <div>
-                {/* <button onClick={() => navigate(-1)}>
+  return (
+    <div className="singleBook">
+      <div className="singleBook">
+        <p>This is the page for a single book with its complete details</p>
+        <p> Book Title: {title} </p>
+        <p> Author: {} </p>
+        <p> Book Language: {} </p>
+        <p> Owner: {} </p>
+        <p> ISBN: {} </p>
+        <p> Age Range: {} </p>
+        <p> Available: {} </p>
+        <p> Publication Year: {} </p>
+        <p> Location: {} </p>
+        <p> Comments: {} </p>
+        <button>Request Book</button>
+      </div>
+      <div>
+        {/* <button onClick={() => navigate(-1)}>
                     Back to books
                 </button> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookSingle;
