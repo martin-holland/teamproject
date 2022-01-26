a7ec5bb61c49e7aabe804527ba6d77d5a54fb60a
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-01-26 13:36:45 +0200

    fix

--- src/components/BookSingle.js
+++ src/components/BookSingle.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import "./BookSingle.css";
-=======
 import React from "react";
->>>>>>> ef4cdeb283f18ef0d6ec3feb4e8a2c193d301dba
 // import { useParams, useNavigate } from 'react-router-dom';
 
 const BookSingle = ({
@@ -19,20 +14,6 @@ const BookSingle = ({
   // let {book} = useParams();
   // let navigate = useNavigate();
 
-<<<<<<< HEAD
-    return (
-        <div className="singleBook">
-            <div className="single_Book">
-                <p>Contact the owner of the book:</p>
-                <textarea id="story" name="story" rows="5" cols="33">
-                </textarea>
-                <button className="send_message">
-                SEND MESSAGE
-                </button>
-            </div>
-            <div>
-                {/* <button onClick={() => navigate(-1)}>
-=======
   return (
     <div className="singleBook">
       <div className="single_Book">
@@ -42,7 +23,6 @@ const BookSingle = ({
       </div>
       <div>
         {/* <button onClick={() => navigate(-1)}>
->>>>>>> ef4cdeb283f18ef0d6ec3feb4e8a2c193d301dba
                     Back to books
                 </button> */}
       </div>
